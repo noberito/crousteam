44810cd5499f0428fd4b160b0006826994cfafdb
Simon Le Cam <simon.le_cam@etu.minesparis.psl.eu>
2024-01-19 17:03:11 +0100

    Fin_vendredi_ajout_tables_profile_preferences

--- back-end/drop.sql
+++ back-end/drop.sql
@@ -1,2 +1,8 @@
 -- cleanup *all* app tables
 DROP TABLE IF EXISTS Auth CASCADE;
+
+DROP TABLE IF EXISTS Profile CASCADE;
+
+DROP TABLE IF EXISTS Pref_of_user CASCADE;
+
+DROP TABLE IF EXISTS Preferences CASCADE;
