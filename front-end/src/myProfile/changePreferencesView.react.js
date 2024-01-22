cc1af5ab285b2f683d2ae1d02b0adf5d5d232ce9
Clément Nober <clement.nober@etu.minesparis.psl.eu>
2024-01-22 16:58:29 +0100

    add changepreferences untracked

---
+++ front-end/src/myProfile/changePreferencesView.react.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import { Text, Button, View, StyleSheet, } from 'react-native';
+
+export default function ChangePreferencesView(){
+    return(
+        <View>
+            <Text> Change Preferences </Text>
+        </View>
+    )
+}
