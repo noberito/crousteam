adff9dffa34f623fe1cfdf4d246ac725698a72a3
Simon Le Cam <simon.le_cam@etu.minesparis.psl.eu>
2024-01-22 09:58:48 +0100

    For_merge

--- back-end/create.sql
+++ back-end/create.sql
@@ -2,8 +2,8 @@
 -- Authentication Data
 --
 -- *MUST* be consistent with "data.sql" import and "test_users.in"
-CREATE TABLE IF NOT EXISTS Auth(
-  lid SERIAL8 PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS AppUser(
+  uid SERIAL8 PRIMARY KEY,
   login TEXT UNIQUE NOT NULL
     CHECK (LENGTH(login) >= 3 AND login ~ E'^[a-zA-Z][-a-zA-Z0-9_@\\.]*$'),
   email TEXT DEFAULT NULL CHECK (email IS NULL OR email ~ E'@'),
@@ -19,16 +19,16 @@ CREATE TABLE IF NOT EXISTS Preferences(
 
 CREATE TABLE IF NOT EXISTS Profile(
   pid SERIAL8 PRIMARY KEY,
-  lid INTEGER NOT NULL,
-  date_naissance TIMESTAMP NOT NULL,
+  uid INTEGER NOT NULL,
+  naissance DATE NOT NULL,
   photo TEXT DEFAULT NULL,
-  CONSTRAINT fk_auth
-    FOREIGN KEY (lid) 
-      REFERENCES Auth(lid)
+  CONSTRAINT fk_user
+    FOREIGN KEY (uid) 
+      REFERENCES AppUser(uid)
       ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS Pref_of_user(
+CREATE TABLE IF NOT EXISTS UsersPref(
   pid INTEGER NOT NULL,
   pfid INTEGER NOT NULL,
   CONSTRAINT fk_profile
@@ -39,4 +39,54 @@ CREATE TABLE IF NOT EXISTS Pref_of_user(
     FOREIGN KEY (pfid)
       REFERENCES Preferences (pfid)
       ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS EventType (
+  tid SERIAL8 PRIMARY KEY,
+  tname TEXT NOT NULL,
+  UNIQUE(tname)
+);
+
+CREATE TABLE IF NOT EXISTS AppGroup(
+  gid SERIAL8 PRIMARY KEY,
+  gname TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS UsersInGroup(
+  gid INTEGER NOT NULL,
+  pid INTEGER NOT NULL,
+  UNIQUE(gid, pid),
+  CONSTRAINT fk_group
+    FOREIGN KEY (gid)
+    REFERENCES AppGroup (gid)
+    ON DELETE CASCADE,
+  CONSTRAINT fk_profile
+    FOREIGN KEY (pid)
+    REFERENCES Profile (pid)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Event(
+  eid SERIAL8 PRIMARY KEY,
+  ename TEXT NOT NULL,
+  eloc TEXT NOT NULL,
+  etime TIMESTAMP NOT NULL,
+  tid INTEGER NOT NULL,
+  gid INTEGER NOT NULL,
+  UNIQUE (ename, eloc, etime),
+  UNIQUE (gid)
+);
+
+CREATE TABLE IF NOT EXISTS UsersInEvent(
+  eid INTEGER NOT NULL,
+  uid INTEGER NOT NULL,
+  UNIQUE (eid, uid),
+  CONSTRAINT fk_event
+    FOREIGN KEY (eid)
+    REFERENCES Event (eid)
+    ON DELETE CASCADE,
+  CONSTRAINT fk_user
+    FOREIGN KEY (uid)
+    REFERENCES AppUser (uid)
+    ON DELETE CASCADE
 );
