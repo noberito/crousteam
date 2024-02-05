833c0b633a90f311336d2ac8a766253bb887c7c8
Clément Nober <clement.nober@etu.minesparis.psl.eu>
2024-02-05 10:50:34 +0100

    commit

--- front-end/src/listEvent/ListEventView.react.js
+++ front-end/src/listEvent/ListEventView.react.js
@@ -1,4 +1,4 @@
-import React, {useContext, useCallback, useState} from 'react';
+import React, {useContext, useCallback, useState, useEffect} from 'react';
 import { View, Text, FlatList, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import BottomBar from '../friender/BottomBar.react';
 import AppContext from '../common/appcontext';
@@ -27,7 +27,44 @@ const styles = StyleSheet.create({
 
 
 export default function ListEventView({ }) {
-    const {page, setPage} = useContext(AppContext)
+    const {page, setPage, authToken} = useContext(AppContext)  
+
+    const [events, setUsers] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [refreshing, setRefreshing] = useState(false);
+    const [hasPermissionError, setPermissionError] = useState(false);
+
+  const getAllPossibleFriendsRequest = useCallback(() => {
+    setIsLoading(true);
+    // Set refreshing to true when we are loading data on pull to refresh
+    axios({
+      baseURL : baseUrl,
+      url : '/event',
+      method : 'GET',
+      headers : { Authorization : 'Bearer ' + authToken},
+    }).then(response => {
+      setIsLoading(false);
+      setRefreshing(false); // Set refreshing to false when data is loaded
+      if (response.status == 200) {
+        const parsedData = response.data.map(event => ({
+          eid: event[0], bio: event[1]
+        }));
+        console.log(parsedData);
+        setUsers(parsedData);
+        setPermissionError(false);
+      } else if(response.status == 403) {
+        setPermissionError(true);
+      }
+    }).catch(err => {
+      console.error(`Something went wrong ${err.message}`);
+      setIsLoading(false);
+      setRefreshing(false); 
+    });
+  }, [authToken]);
+
+  useEffect(() => {
+    getAllPossibleFriendsRequest();
+  }, [authToken, getAllPossibleFriendsRequest]);
 
     return (
         <View style={styles.mainContainer}>
