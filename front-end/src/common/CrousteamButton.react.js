728ecf5f86dc21780554aed108734fd631ccb8cb
Paul Padilla <paul.padilla@etu.minesparis.psl.eu>
2024-02-07 16:26:58 +0100

    Crousteam button properties

--- front-end/src/common/CrousteamButton.react.js
+++ front-end/src/common/CrousteamButton.react.js
@@ -1,37 +1,35 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Text, Button, View, StyleSheet, TouchableOpacity } from 'react-native';
 import colors from './Colors.react';
 
 const buttonStyles = StyleSheet.create({
   buttonContainer: {
-    width:'50%',
     backgroundColor: colors.background,
     borderRadius: 10,
     padding: 20,
-    justifyContent:'center',
-    alignItems:'center',
+    justifyContent: 'center',
+    alignItems: 'center',
     margin: 20,
     elevation: 3, // for Android
-    shadowColor: colors.secondaryText, 
-    shadowOffset: { width: 0, height: 7 }, 
+    shadowColor: colors.secondaryText,
+    shadowOffset: { width: 0, height: 7 },
   },
   buttonText: {
     color: colors.primaryText,
-    fontSize: 24,
-    fontFamily:'Arista-Pro-Alternate-Bold-trial',
+    fontFamily: 'Arista-Pro-Alternate-Bold-trial',
   },
-  center:{
-    justifyContent:'center',
-    alignItems:'center',
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 });
 
-const CrousteamButton = ({ onPress, title }) => {
-    return (
-      <TouchableOpacity onPress={onPress} style={buttonStyles.buttonContainer}>
-          <Text style={buttonStyles.buttonText}>{title}</Text>
-      </TouchableOpacity>
-    );
+const CrousteamButton = ({ onPress, title, styleText }) => {
+  return (
+    <TouchableOpacity onPress={onPress} style={buttonStyles.buttonContainer}>
+      <Text style={[buttonStyles.buttonText, styleText]}>{title}</Text>
+    </TouchableOpacity>
+  );
 
 };
 
