f95ad775ce405ac5ede8ff91d5de4fabd0e1ee03
Clément Nober <clement.nober@etu.minesparis.psl.eu>
2024-02-09 15:22:08 +0100

    changes

--- front-end/src/listEvent/AllEvents.react.js
+++ front-end/src/listEvent/AllEvents.react.js
@@ -11,11 +11,7 @@ const styles = StyleSheet.create({
 
 
 
-<<<<<<< HEAD
-export default function AllEvents({ eid, setEid, log, setLog }) {
-=======
 export default function AllEvents({eid, setGid, log, setLog}){
->>>>>>> a70b53c (post event erreur 400 solved, but error 500)
 
   const { page, setPage, authToken } = useContext(AppContext)
 
@@ -37,11 +33,7 @@ export default function AllEvents({eid, setGid, log, setLog}){
       setRefreshing(false); // Set refreshing to false when data is loaded
       if (response.status == 200) {
         const parsedData = response.data.map(event => ({
-<<<<<<< HEAD
-          eid: event[0], title: event[1], loc: event[2], time: event[3], description: event[4]
-=======
           title:event[0], loc: event[1], date:event[2], duration:event[3], description:event[4], gid:event[5]
->>>>>>> a70b53c (post event erreur 400 solved, but error 500)
         }));
         console.log(parsedData);
         setEvents(parsedData);
@@ -59,24 +51,6 @@ export default function AllEvents({eid, setGid, log, setLog}){
   useEffect(() => {
     getAllEvents();
   }, [authToken, getAllEvents]);
-<<<<<<< HEAD
-
-
-  const renderEventItem = ({ item }) => { return (<EventIcon item={item} setEid={setEid}></EventIcon>) }
-
-  return (
-    <View>
-      {isLoading && <ActivityIndicator size='large' animating={true} color='#FF0000' />}
-      <FlatList
-        data={events}
-        keyExtractor={(item) => item.eid}
-        renderItem={renderEventItem}
-        numColumns={2}
-      />
-    </View>
-  )
-};
-=======
     
     
     const renderEventItem = ({item}) => {return(<EventIcon item={item} setGid={setGid}></EventIcon>)}
@@ -92,4 +66,3 @@ export default function AllEvents({eid, setGid, log, setLog}){
             />
         </View>
 )};
->>>>>>> a70b53c (post event erreur 400 solved, but error 500)
