74f46423cefca44ca3e45fca6f78cc97d64ef7f0
abdallah melhem <abdallah.melhem@etu.minesparis.psl.eu>
2024-02-12 09:28:14 +0100

    added profile photo addition on backend side

--- back-end/querie.py
+++ back-end/querie.py
@@ -1,7 +1,6 @@
-import anodb  # type: ignore
 import json
 from datetime import datetime
-
+from pathlib import Path
 
 # Fonction pour convertir les objets datetime en chaînes ISO 8601
 def datetime_converter(o):
@@ -10,29 +9,18 @@ def datetime_converter(o):
             i = i.isoformat()
 
 
-db = anodb.DB("postgres", "dbname=crousteam", "queries.sql")
-login = "calvin"
-list_pftype = ["amateur de cinema", "philantropique", "cowboy"]
+def get_image_path(filename):
+    # log.debug(filename)
+    # Get the path of the image file
+    file_name = Path(filename)
+    if file_name.exists():
+        return filename, 200
+    # file_path = filename
+    # Check if the file exists
+    # if os.path.exists(file_path):
+    # Return the file path
 
-ename = "Bigman_streep"
-eloc = "Paris"
-etime = "2024-04-25"
-edate = "10:00:00"
-eduree = "03:00:00"
+    else:
+        return "File not found", 404
 
-
-# gid = db.create_group_chat_link_to_the_event(ename=ename)
-# eid = db.add_event(
-#     ename=ename, eloc=eloc, edate=edate, etime=etime, eduree=eduree, gid=int(gid)
-# )
-# lid = db.get_lid_from_login(login="hobbes")
-# db.add_people_into_group_ecreator(gid=gid, lid=lid)
-res_login = db.get_event_info(gid=3)
-# res_login2 = db.test()
-
-# (preferences_list=json.dumps(list_pftype))
-# res_login = db.test()
-if res_login:
-    # res_login = json.dumps(list(res_login), default=datetime_converter)
-    print(f"res_login={json.dumps(list(res_login))}")
-    # print(f"res_login={json.dumps(list(res_login2))}")
+print(get_image_path("/home/mobapp/crousteam/back-end/0e31eb6d-32f3-4066-b3ff-7a70e7b05d98.text"))
